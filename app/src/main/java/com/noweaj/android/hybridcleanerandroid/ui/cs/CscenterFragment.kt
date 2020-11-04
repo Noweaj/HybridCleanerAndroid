f2bafdaec208e3bb739a2508c6262e5f23671090
noweaj <noweaj.dev@gmail.com>
2020-11-05 01:19:22 +0900

    Added call center fragment

--- app/src/main/java/com/noweaj/android/hybridcleanerandroid/ui/cs/CscenterFragment.kt
+++ app/src/main/java/com/noweaj/android/hybridcleanerandroid/ui/cs/CscenterFragment.kt
@@ -1,6 +1,27 @@
 package com.noweaj.android.hybridcleanerandroid.ui.cs
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.noweaj.android.hybridcleanerandroid.databinding.FragmentCsCenterBinding
 
 class CscenterFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentCsCenterBinding.inflate(inflater, container, false)
+        binding.ibCsCenterCall.setOnClickListener{
+            val callIntent = Intent(Intent.ACTION_DIAL)
+            callIntent.setData(Uri.parse("tel:15771577"))
+            startActivity(callIntent)
+        }
+        return binding.root
+    }
 }
